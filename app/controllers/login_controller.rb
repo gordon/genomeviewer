cff16007e46401889a324362721fd28ab31f9d98
Giorgio Gonnella <ggonnella@yahoo.it>
2007-11-17 21:50:29 +0100

    updated login controller to use the standard layout

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,16 @@
 class LoginController < ApplicationController
+  
+ def login
+   @page_title = "Genomeviewer - User Login"
+ end
+ def login_fail
+   @page_title = "Login Failed"
+ end
+ def login_ok
+   @page_title = "Login Successful"
+  end
+ 
+ 
  def do_login
   if User.find_by_login_and_password(params[:user], params[:pass])
    session[:user]=params[:user]
