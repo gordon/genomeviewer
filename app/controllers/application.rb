2a8c12dfa4be74ecf246a9519bad04c50f0531de
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-12 18:12:56 +0100

    correction to last commit
    (1) The file app/controllers/application.rb was not in the right version
    (2) The commit msg should have been:
    
    generation of png images fixed: a stream is sent directly to the browser
    
     - 1 - the SequenceRegion#to_png method now returns the png data
           and not a filename anymore. Please note that until the genometools
           will require to save the data in a file, this method will have
           to save the data in a tmp file (which is then delete) and then
           read the data again (that will be sent directly to the browser
           from the controller, see point 2). This, however, impacts the performance.
    
     - 2 - an action png_image was added to (for the moment) application.rb
           [Marvin, please move it later to your browser controller] and
           calls SequenceRegion#to_png with the right parameters (from the
           params hash), sending the png data directly to the browser
    
     - 3 - an example of application is app/views/in_session/view.html.erb
    
           image_tag(url_for(:action=>:png_image,
                             :annotation=>params[:annotation],
                             :sequence_region=>params[:sequence_region],
                             :seq_begin=>params[:seq_begin],
                             :seq_end=>params[:seq_end]))
    
           [the view was rewritten to use the new methods, although it will be
           replaced, as an example usage; now it correctly moves right and left
           and zooms, however it shows always the first sequence_region (see _navbar)

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -5,10 +5,17 @@ class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_genomeviewer_session_id'
   
+  # sends the png data directly to the browser
+  #
+  # can be moved to the controller that really uses it
+  # 
+  # usage: image_tag(url_for(:action =>:png_image, :annotation => ..., 
+  #                          :sequence_region => ..., :seq_begin => ..., :seq_end => ...)
   def png_image
     annotation=Annotation.find(params[:annotation])
-    sequence_region=annotation.sequence_regions[0]
-    send_data(sequence_region.to_png, 
+    sequence_region=SequenceRegion.find(params[:sequence_region])
+    png_data = sequence_region.to_png(params[:seq_begin].to_i,params[:seq_end].to_i)
+    send_data(png_data, 
              :type => "image/png", 
              :disposition => "inline", 
              :filename => "#{annotation.name}_#{sequence_region.seq_id}.png")
