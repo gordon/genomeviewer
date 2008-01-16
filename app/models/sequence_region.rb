e62f1b9d00b1169863ec8e189080547c45633809
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-16 12:58:48 +0100

    modified sequence_region model to use png_stream
     * the png data is not anymore saved in a temp file
       but sent directly to the browser
    
     * an optional width parameter is added to .to_png
       with a default of 800 pixel

--- app/models/sequence_region.rb
+++ app/models/sequence_region.rb
@@ -1,46 +1,28 @@
 class SequenceRegion < ActiveRecord::Base
   belongs_to :annotation
   
-  # returns the png image representing the sequence region
+  # returns the png image representing the sequence region using the gt-ruby bindings
   def to_png(seq_begin = self.seq_begin, seq_end = self.seq_end)
-   temp_file = "tmp/images/#{self.annotation.id}_#{self.id}_#{self.seq_begin}_#{self.seq_end}.png"  
-   if generate_png(seq_begin, seq_end, temp_file)   
-     png_data = IO.read(temp_file)
-     File.delete(temp_file)
-     return png_data
-   else
-     return nil
-   end
-  end
-
-  private
-  
-  # use the genometools ruby bindings to generate the png file for the to_png method
-  def generate_png(seq_begin, seq_end, output_file)
-    begin
-        in_stream = GT::GFF3InStream.new(self.annotation.gff3_data_storage)
-        feature_index = GT::FeatureIndex.new
-        feature_stream = GT::FeatureStream.new(in_stream, feature_index)
-        gn = feature_stream.next_tree
-        # fill feature index
-        while (gn) do
-          gn = feature_stream.next_tree
-        end
-
-        seqid = feature_index.get_first_seqid
-        range = feature_index.get_range_for_seqid(seqid)
-        range.start = seq_begin
-        range.end = seq_end
-
-        config = GT::Config.new
-        diagram = GT::Diagram.new(feature_index, seqid, range, config)
-        
-        render = GT::Render.new(config)
-        render.to_png(diagram, output_file)
-        return true
-    rescue
-        return false
+	p '====================='
+	p self[:seq_id]
+    in_stream = GT::GFF3InStream.new(self.annotation.gff3_data_storage)
+    feature_index = GT::FeatureIndex.new
+    feature_stream = GT::FeatureStream.new(in_stream, feature_index)
+    gn = feature_stream.next_tree
+    # fill feature index
+    while (gn) do
+      gn = feature_stream.next_tree
     end
+
+    range = feature_index.get_range_for_seqid(seq_id)
+    range.start = seq_begin
+    range.end = seq_end
+
+    config = GT::Config.new
+    diagram = GT::Diagram.new(feature_index, seq_id, range, config)
+    render = GT::Render.new(config)
+
+    return render.to_png_stream(diagram)
   end
 
 end
