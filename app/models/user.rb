156722fd04c36cc0dfb81fe067bc3ac0f6c546da
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-23 23:32:17 +0100

    added content to the config method of User model
     - now every kind of user specific configurations are loaded
       in the config object before it is returned

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :message => "Please enter your email address"
   validates_presence_of :password, :message => "Please choose a password"
   validates_length_of :name, :in => 4..64, :too_short => "Your name should be at least %d characters long", :too_long => "Please enter a name shorter than %d characters"
-  validates_length_of :email, :maximum => 64, :too_long => "The ente:red email address is too long (max 64 chars)"
+  validates_length_of :email, :maximum => 64, :too_long => "The entered email address is too long (max 64 chars)"
   validates_confirmation_of :password, :message => "You entered two different passwords!"
   validates_format_of :email, :with => /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i, :message => 'Email address invalid'
 
@@ -31,6 +31,42 @@ class User < ActiveRecord::Base
     c = GT::Config.new
     # load default configuration
     c.load_file("config/view.lua")
+    # load user specific colors
+    color_configurations.each do |record|
+      color = GT::Color.malloc
+      color.red    = record.red.to_f
+      color.green = record.green.to_f
+      color.blue   = record.blue.to_f
+      c.set_color(record.element.name,color)
+    end
+    # load user specific feature styles
+    feature_style_configurations.each do |record|
+      c.set_cstr("feature_styles",record.feature_class.name,record.style.name)
+    end
+    # load user specific formats
+    if drawing_format_configuration
+      # set show_grid
+      c.set_num("format","show_grid", drawing_format_configuration.show_grid ? 1.0 : 0.0)
+      # set all other format attributes 
+      format_attributes = (drawing_format_configuration.attribute_names - ["user_id","show_grid"])
+      format_attributes.each do |attribute|
+        c.set_num("format",attribute,drawing_format_configuration.send(attribute).to_f)
+      end
+    end
+    # create an hash with the domination data
+    dominations = {}
+    domination_configurations.each do |record|
+      dominations[record.dominating.name] ||= []
+      dominations[record.dominating.name].push record.dominated.name
+    end
+    # use the dominations hash to load user specific domination data
+    dominations.each_pair do |dominating, list|
+      c.set_cstr_list("dominate", dominating, list)
+    end
+    # load collapsing configuration
+    if collapsing_configuration
+      c.set_cstr_list("collapse", "to_parent", collapsing_configuration.to_parent)
+    end
     return c
   end
     
