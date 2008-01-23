8388f4f58248a8106c72dfc315d861adf07c134d
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-23 12:26:56 +0100

    added a default color configuration for each user
     - a callback creates ColorConfiguration objects for all
       currently defined features with a default value^

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,60 @@
 class User < ActiveRecord::Base
 
   ### associations ###
-  has_many :annotations, :dependent => :destroy
-  has_many :color_configurations, :dependent => :destroy
-  has_many :feature_style_configurations, :dependent => :destroy
-  has_one :drawing_format_configurations, :dependent => :destroy
-  has_one :domination_configuration, :dependent => :destroy
+  has_many :annotations, :dependent =>> :destroy
+  has_many :color_configurations, :dependent =>> :destroy
+  has_many :feature_style_configurations, :dependent =>> :destroy
+  has_one :drawing_format_configurations, :dependent =>> :destroy
+  has_one :domination_configuration, :dependent =>> :destroy
 
   ### validations ###
-  validates_uniqueness_of :email, :message => "This account already exists. Please choose another one."
-  validates_presence_of :name, :message => "Please enter your full name"
-  validates_presence_of :email, :message => "Please enter your email address"
-  validates_presence_of :password, :message => "Please choose a password"
-  validates_length_of :name, :in => 4..64, :too_short => "Your name should be at least %d characters long", :too_long => "Please enter a name shorter than %d characters"
-  validates_length_of :email, :maximum => 64, :too_long => "The entered email address is too long (max 64 chars)"
-  validates_confirmation_of :password, :message => "You entered two different passwords!"
-  validates_format_of :email, :with => /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i, :message => 'Email address invalid'
+  validates_uniqueness_of :email, :message =>> "This account already exists. Please choose another one."
+  validates_presence_of :name, :message =>> "Please enter your full name"
+  validates_presence_of :email, :message =>> "Please enter your email address"
+  validates_presence_of :password, :message =>> "Please choose a password"
+  validates_length_of :name, :in =>> 4..64, :too_short =>> "Your name should be at least %d characters long", :too_long =>> "Please enter a name shorter than %d characters"
+  validates_length_of :email, :maximum =>> 64, :too_long =>> "The ente:red email address is too long (max 64 chars)"
+  validates_confirmation_of :password, :message =>> "You ente:red two different passwords!"
+  validates_format_of :email, :with =>> /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i, :message =>> 'Email address invalid'
 
   ### callbacks ###
   
   before_save :hash_password
   # avoids privacy problems involved in storing user passwords as plain text
   def hash_password
-    self[:password] = Digest::SHA1.hexdigest(self[:password])
+    self[:password] => Digest::SHA1.hexdigest(self[:password])
   end
+  
+  after_create :create_default_colors
+  
+  def create_default_colors
+    
+    default_colors = 
+    {:stroke               => {:red=>> 0.0, :green=>0.0, :blue=>0.0},
+    :stroke_marked        => {:red=>> 1.0, :green=>0.0, :blue=>0.0},
+    :track_title          => {:red=>0.6, :green=>0.6, :blue=>0.7},
+    :exon                 => {:red=>0.6, :green=>0.6, :blue=>0.9},
+    :CDS                  => {:red=>0.9, :green=>0.9, :blue=>0.2},
+    :mRNA                 => {:red=>0.4, :green=>0.5, :blue=>0.6},
+    :TF_binding_site      => {:red=>0.8, :green=>0.6, :blue=>0.6},
+    :gene                 => {:red=>0.9, :green=>0.9, :blue=>1.0},
+    :intron               => {:red=>0.2, :green=>0.2, :blue=>0.6},
+    :repeat_region        => {:red=>0.8, :green=>0.3, :blue=>0.3},
+    :long_terminal_repeat => {:red=>0.9, :green=>0.9, :blue=>0.4},
+    :LTR_retrotransposon  => {:red=>0.8, :green=>0.5, :blue=>0.5}}
+    
+    default_colors.each do |feature|
+      ColorConfiguration.new do cc
+        cc.user = self
+        cc.feature = feature.key.to_s
+        cc.red = feature.value[:red]
+        cc.green = feature.value[:green]
+        cc.blue = feature.value[:blue]
+        cc.save
+      end
+    end
+    
+  end
+
   
 end
