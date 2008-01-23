19b795496dfbf9526aa1866aa5c8f8bbcc5a2a37
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-23 19:03:33 +0100

    in feature style table "feature_id" column renamed in "feature_class_id"

--- db/migrate/008_create_feature_style_configurations.rb
+++ db/migrate/008_create_feature_style_configurations.rb
@@ -2,7 +2,7 @@ class CreateFeatureStyleConfigurations < ActiveRecord::Migration
   def self.up
     create_table :feature_style_configurations do |t|
       t.belongs_to :user
-      t.references :feature
+      t.references :feature_class
       t.references :style
     end
   end
