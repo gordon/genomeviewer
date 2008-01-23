840ae69bd922530846f970f42fcb60ed91c64771
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-23 12:00:21 +0100

    Added a model for configuration of colors
     - is a generic table that does not specify the name of the feautures
     - each record belongs_to an user and specify the color for one kind of feature

---
+++ app/models/color_configuration.rb
@@ -0,0 +1,2 @@
+class ColorConfiguration < ActiveRecord::Base
+end
---
+++ db/migrate/004_create_color_configurations.rb
@@ -0,0 +1,15 @@
+class CreateColorConfigurations < ActiveRecord::Migration
+  def self.up
+    create_table :color_configurations do |t|
+      t.belongs_to :user
+      t.string :feature
+      t.decimal :red
+      t.decimal :green
+      t.decimal :blue
+    end
+  end
+
+  def self.down
+    drop_table :color_configurations
+  end 
+end
