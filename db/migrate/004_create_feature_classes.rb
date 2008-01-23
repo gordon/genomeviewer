2309984e8fe105d7abac1bfd491f31deefa6d8b3
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-23 18:49:33 +0100

    added content to the feature_class migration
     - some common feature classes are now created during
       the migration

--- db/migrate/004_create_feature_classes.rb
+++ db/migrate/004_create_feature_classes.rb
@@ -1,9 +1,21 @@
 class CreateFeatureClasses < ActiveRecord::Migration
   def self.up
     create_table :feature_classes do |t|
-
-      t.timestamps
+      t.string :name
     end
+    
+    FeatureClass.create(:name => "gene")
+    FeatureClass.create(:name => "exon")
+    FeatureClass.create(:name => "intron")
+    FeatureClass.create(:name => "CDS")
+    FeatureClass.create(:name => "mRNA")
+    FeatureClass.create(:name => "TF_binding_site")
+    FeatureClass.create(:name => "repeat_region")
+    FeatureClass.create(:name => "long_terminal_repeat")
+    FeatureClass.create(:name => "LTR_retrotransposon")
+    FeatureClass.create(:name => "inverted_repeat")
+    FeatureClass.create(:name => "target_site_duplication")
+    
   end
 
   def self.down
