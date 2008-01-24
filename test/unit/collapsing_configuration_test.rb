b1dbf0031539d728158a68354333035b97135cf5
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-24 01:35:12 +0100

    added unit test for collapsing configuration

--- test/unit/collapsing_configuration_test.rb
+++ test/unit/collapsing_configuration_test.rb
@@ -1,8 +1,17 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class CollapsingConfigurationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  
+  fixtures :users
+  
+  def test_load_collapsing_configuration
+    giorgio = users("giorgio")
+    cc = CollapsingConfiguration.new
+    cc.to_parent = ["exon","intron"]
+    giorgio.collapsing_configuration = cc
+    assert giorgio.save
+    assert_equal cc.to_parent, 
+                      giorgio.config.get_cstr_list("collapse", "to_parent").to_a
   end
+  
 end
