6b2ab5254eb5b32cc5ff065433610d30b5e34ebf
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-24 01:37:08 +0100

    added unit test for drawing format configuration

--- test/unit/drawing_format_configuration_test.rb
+++ test/unit/drawing_format_configuration_test.rb
@@ -1,8 +1,48 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class DrawingFormatConfigurationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  fixtures :users
+  
+  def test_load_drawing_formats
+    giorgio = users("giorgio")
+    d = DrawingFormatConfiguration.new
+    giorgio.drawing_format_configuration = d
+    assert_not_equal giorgio.config.get_num("format","arrow_width"),
+                            10.0
+    d.arrow_width = 10
+    assert_equal giorgio.config.get_num("format","arrow_width"),
+                            10.0
+    assert_not_equal giorgio.config.get_num("format","bar_height"),
+                            11.0
+    d.bar_height = 11
+    assert_equal giorgio.config.get_num("format","bar_height"),
+                            11.0
+    assert_not_equal giorgio.config.get_num("format","margins"),
+                            13.0
+    d.margins = 13
+    assert_equal giorgio.config.get_num("format","margins"),
+                            13.0
+    assert_not_equal giorgio.config.get_num("format","scale_arrow_height"),
+                            1.1
+    d.scale_arrow_height = 1.1
+    assert_equal giorgio.config.get_num("format","scale_arrow_height"),
+                            1.1
+    assert_not_equal giorgio.config.get_num("format","scale_arrow_width"),
+                            1.2
+    d.scale_arrow_width = 1.2
+    assert_equal giorgio.config.get_num("format","scale_arrow_width"),
+                            1.2
+    assert_not_equal giorgio.config.get_num("format","show_grid"),
+                            0.0
+    d.show_grid = false
+    assert_equal giorgio.config.get_num("format","show_grid"),
+                            0.0
+    assert_not_equal giorgio.config.get_num("format","stroke_width"),
+                            1.4
+    d.stroke_width = 1.4
+    assert_equal giorgio.config.get_num("format","stroke_width"),
+                            1.4    
   end
+
 end
