758d172d2d3d12014ba5ea95cec02616b8f9315e
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-24 22:48:17 +0100

    added a method to read default colors from standard lua file
     - this is useful for the config template

--- app/models/color_configuration.rb
+++ app/models/color_configuration.rb
@@ -3,4 +3,24 @@ class ColorConfiguration < ActiveRecord::Base
   belongs_to :element, :polymorphic => true
   belongs_to :user
   
+  # returns an hash with the default values from view.lua
+  def self.defaults
+    c = GT::Config.new
+    c.load_file("config/view.lua")
+    colors = {}
+    # as there is no iterator yet in gtruby 
+    # try all features and graphical elements
+    elements = FeatureClass.find(:all) + 
+                  GraphicalElement.find(:all)
+    elements.map(&:name).each do |e| 
+      # if e did not exist 0.8 is returned for all colors
+      # (default gray color)
+      colors[e] = {}
+      [:red, :green, :blue].each do |rgb|
+        colors[e][rgb] = c.get_color(e).send(rgb)
+      end
+    end
+    return colors
+  end
+  
 end
