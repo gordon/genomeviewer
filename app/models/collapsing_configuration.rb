240ea60fbecd4cdd725ecaaceffb2ae745d40c26
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-25 00:41:39 +0100

    implemented a default class method also for collapsing_configuration
     it simply returns an array (the to_parent)

--- app/models/collapsing_configuration.rb
+++ app/models/collapsing_configuration.rb
@@ -2,4 +2,12 @@ class CollapsingConfiguration < ActiveRecord::Base
   
   serialize :to_parent, Array
   
+  # returns the default value as array
+  # reads it from view.lua using gtruby
+  def self.default    
+    c = GT::Config.new
+    c.load_file("config/view.lua")
+    return c.get_cstr_list("collapse","to_parent").to_a
+  end
+  
 end
