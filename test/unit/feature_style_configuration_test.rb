39b91fe611b215bf5ebd370d3d04f7ca8921787d
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-24 01:34:32 +0100

    added unit test for feature style configuration

--- test/unit/feature_style_configuration_test.rb
+++ test/unit/feature_style_configuration_test.rb
@@ -1,8 +1,19 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class FeatureStyleConfigurationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  fixtures :users, :feature_classes, :styles
+  
+  def test_load_feature_styles
+    giorgio = users("giorgio")
+    assert_not_equal giorgio.config.get_cstr("feature_styles","gene"),
+                     "dashes"
+    f = FeatureStyleConfiguration.new
+    f.feature_class = feature_classes("gene")
+    f.style = styles("dashes")
+    giorgio.feature_style_configurations << f
+    assert_equal giorgio.config.get_cstr("feature_styles","gene"),
+                     "dashes"
   end
+
 end
