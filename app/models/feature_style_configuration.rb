9a9f6d36b3effa8f528dab26b7584bcebd5030f5
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-24 22:57:48 +0100

    added a #defaults class method to FeatureStyleConfigurations
      - reads the default values from view.lua using the ruby bindings
      - returns the values as hash

--- app/models/feature_style_configuration.rb
+++ app/models/feature_style_configuration.rb
@@ -3,5 +3,18 @@ class FeatureStyleConfiguration < ActiveRecord::Base
   belongs_to :user
   belongs_to :style
   belongs_to :feature_class
+  
+  # returns an hash with the default values from view.lua
+  def self.defaults
+    c = GT::Config.new
+    c.load_file("config/view.lua")
+    styles = {}
+    # as there is no iterator yet in gtruby try all features 
+    FeatureClass.find(:all).map(&:name).each do |f| 
+      f_style = c.get_cstr("feature_styles",f)
+      styles[f] = f_style unless f_style == "undefined"
+    end
+    return styles
+  end
 
 end
