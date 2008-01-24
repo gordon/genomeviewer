6325b474a479c46b08c92fd36b05980ed5f9ec79
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-24 01:35:49 +0100

    added unit test for color configuration

--- test/unit/color_configuration_test.rb
+++ test/unit/color_configuration_test.rb
@@ -1,8 +1,25 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ColorConfigurationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  fixtures :users,
+             :feature_classes
+  
+  def test_load_color_configurations
+    user = users("giorgio")
+    cc = ColorConfiguration.new
+    cc.element = feature_classes("exon")
+    cc.red = 0
+    cc.green = 0.4
+    cc.blue = 1
+    assert_not_equal cc.red.to_f, user.config.get_color("exon").red
+    assert_not_equal cc.green.to_f, user.config.get_color("exon").green
+    assert_not_equal cc.blue.to_f, user.config.get_color("exon").blue    
+    user.color_configurations << cc
+    assert user.save
+    assert_equal cc.red.to_f, user.config.get_color("exon").red
+    assert_equal cc.green.to_f, user.config.get_color("exon").green
+    assert_equal cc.blue.to_f, user.config.get_color("exon").blue
   end
+
 end
