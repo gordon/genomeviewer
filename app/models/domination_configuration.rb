05067959da4c8e31eafbcb0f5a195b7fa9c3f7c9
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-24 23:03:57 +0100

    added a class method defaults also to domination_configuration
     like for the last 2 commits: reads lua file, returns an hash

--- app/models/domination_configuration.rb
+++ app/models/domination_configuration.rb
@@ -4,4 +4,17 @@ class DominationConfiguration < ActiveRecord::Base
   belongs_to :dominated, :class_name => "FeatureClass"
   belongs_to :dominating, :class_name => "FeatureClass"
   
+  # returns the default values from view.lua
+  def self.defaults
+    c = GT::Config.new
+    c.load_file("config/view.lua")
+    dominations = {}
+    # as there is no iterator yet in gtruby try all features 
+    FeatureClass.find(:all).map(&:name).each do |f| 
+      dominated = c.get_cstr_list("dominate",f).to_a
+      dominations[f] = dominated unless dominated.empty?
+    end
+    return dominations
+  end
+  
 end
