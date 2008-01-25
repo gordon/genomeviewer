b1756ac3c54380dfbf25825b77350196fecd1902
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-25 22:09:37 +0100

    collapsing configuration is now operating

--- app/controllers/config/collapse.rb
+++ app/controllers/config/collapse.rb
@@ -8,6 +8,47 @@ module Config::Collapse
       else 
         CollapsingConfiguration.default
       end
+    p user.collapsing_configuration
+    @not_collapsing = 
+      FeatureClass.find(:all).delete_if {|fc| @collapse.include? fc.name}
   end
+    
+  def collapse_remove
+    user = User.find(session[:user])
+    unless user.collapsing_configuration
+      user.collapsing_configuration = CollapsingConfiguration.new 
+      user.collapsing_configuration.to_parent = CollapsingConfiguration.default
+      user.collapsing_configuration.save
+    end
+    user.collapsing_configuration.to_parent.delete(params[:feature_class_name])     
+    user.collapsing_configuration.save
+    if user.collapsing_configuration.to_parent == CollapsingConfiguration.default
+      user.collapsing_configuration = nil
+    end
+    redirect_to :action => :config_collapse
+  end
+  
+  def collapse_add
+    user = User.find(session[:user])
+    unless user.collapsing_configuration
+      user.collapsing_configuration = CollapsingConfiguration.new 
+      user.collapsing_configuration.to_parent = CollapsingConfiguration.default
+      user.collapsing_configuration.save
+    end
+    user.collapsing_configuration.to_parent << 
+                   FeatureClass.find(params[:feature_class][:id]).name  
+    user.collapsing_configuration.save
+    if user.collapsing_configuration.to_parent == CollapsingConfiguration.default
+      user.collapsing_configuration = nil
+    end
+    redirect_to :action => :config_collapse
+  end
+    
+  def reset_collapse_list
+    user = User.find(session[:user])
+    user.collapsing_configuration = nil
+    user.save
+    redirect_to :action => :config_collapse
+  end    
 
 end
