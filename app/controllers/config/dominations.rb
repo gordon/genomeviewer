c05ecae246b01628135c84a1a28ad7ad54430978
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-26 02:36:19 +0100

    now also the domination configuration is operative
     - writing the whole code for the configuration pages
       required a real big effort that is:
    
        - 9 migrations
    
        - 9 models (associations etc) including
          class method "defaults" for each config model
    
        - the config method for the user model
    
        - 24 actions for the in_session_controller
          (divided in 5 modules)
    
        - 8 templates
    
      !!! a total of ca. 750 lines of code !!!
    
      THAT means:
    
        - some code can be made much mode DRY
        - optimizations / more elegant ways can be found in some cases
        - hopefully there are not many bugs, but tests could be written

--- app/controllers/config/dominations.rb
+++ app/controllers/config/dominations.rb
@@ -1,10 +1,9 @@
 module Config::Dominations
 
   def config_dominations
-    user = User.find(session[:user])
     @dominations = DominationConfiguration.defaults
-    user.domination_configurations.each do |conf|
-      @dominations[conf.dominator.name] = conf.dominated_features.map(&:name)
+    User.find(session[:user]).domination_configurations.each do |conf|
+      @dominations[conf.dominator.name] = conf.dominated_features.map(&:feature_class).map(&:name)
     end    
     @not_dominating = 
      FeatureClass.find(:all).delete_if do |fc| 
@@ -13,14 +12,82 @@ module Config::Dominations
   end 
         
   def reset_dominations
-    user = User.find(session[:user])
-    user.domination_configurations.destroy_all
+    User.find(session[:user]).domination_configurations.destroy_all
     redirect_to :action => :config_dominations
   end    
-
-  def manage_dominated
-    user = User.find(session[:user])
-    redirect_to :action => :config_dominations    
+  
+  def add_dominated_list
+    User.find(session[:user]).domination_configurations << 
+      DominationConfiguration.new(:dominator_id => params[:feature_class][:id])
+    redirect_to :action => :manage_dominated_list, 
+                   :dominator => FeatureClass.find(params[:feature_class][:id]).name
   end
   
+  def delete_dominated_list
+    user = User.find(session[:user])
+    dominator = FeatureClass.find_by_name(params[:dominator])
+    dc = DominationConfiguration.find_by_user_id_and_dominator_id(session[:user], dominator.id)
+    if dc 
+      # it was an user list, delete it 
+      dc.destroy
+    else 
+      # it was a default list, create an empty list
+      user.domination_configurations << DominationConfiguration.new(:dominator => dominator)
+    end
+    redirect_to :action => :config_dominations
+  end
+  
+  def reset_dominated_list
+    dominator = FeatureClass.find_by_name(params[:dominator])
+    dc = DominationConfiguration.find_by_user_id_and_dominator_id(session[:user], dominator.id)
+    dc.destroy if dc
+    redirect_to :action => :config_dominations
+  end
+
+  def manage_dominated_list
+    user = User.find(session[:user])
+    @dominator = FeatureClass.find_by_name(params[:dominator])
+    dc = DominationConfiguration.find_by_user_id_and_dominator_id(session[:user], @dominator.id)
+    if dc
+      # an user specific list exists
+      @dominated = dc.dominated_features.map(&:feature_class).map(&:name)
+    else
+      # return a default list or an empty list if no default exists
+      @dominated = DominationConfiguration.defaults.fetch(params[:dominator], [])
+    end
+    @not_dominated = FeatureClass.find(:all).delete_if {|fc| @dominated.include?(fc.name) or fc == @dominator}
+  end
+
+  def dominated_remove
+    user = User.find(session[:user])
+    dominator = FeatureClass.find_by_name(params[:dominator])
+    dc = DominationConfiguration.find_by_user_id_and_dominator_id(session[:user], dominator.id)
+    unless dc # the user had no list 
+      dc = DominationConfiguration.new(:dominator => dominator)    
+      dc.dominated_features << DominationConfiguration.defaults[dominator.name].map do |dominated_fcn|
+          DominatedFeature.new(:feature_class => FeatureClass.find_by_name(dominated_fcn))
+        end
+      user.domination_configurations << dc
+    end
+    record_to_delete = dc.dominated_features.find(:first, 
+                              :conditions => {:feature_class_id => FeatureClass.find_by_name(params[:feature_class_name]).id})
+    dc.dominated_features.delete(record_to_delete)
+    redirect_to :action => :manage_dominated_list, :dominator => params[:dominator]
+  end
+  
+  def dominated_add
+    user = User.find(session[:user])
+    dominator = FeatureClass.find_by_name(params[:dominator])
+    dc = DominationConfiguration.find_by_user_id_and_dominator_id(session[:user], dominator.id)
+    unless dc # the user had no list 
+      dc = DominationConfiguration.new(:dominator => dominator)    
+      dc.dominated_features << DominationConfiguration.defaults[dominator.name].map do |dominated_fcn|
+          DominatedFeature.new(:feature_class => FeatureClass.find_by_name(dominated_fcn))
+        end
+      user.domination_configurations << dc
+    end
+    dc.dominated_features << DominatedFeature.new(:feature_class => FeatureClass.find(params[:feature_class][:id]))
+    redirect_to :action => :manage_dominated_list, :dominator => params[:dominator]
+  end
+
 end
