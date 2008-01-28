f5b5aea7940265e9525adbf43f46c6e719f691a4
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-28 19:41:22 +0100

    added helptext to drawing format configuration page

--- app/models/drawing_format_configuration.rb
+++ app/models/drawing_format_configuration.rb
@@ -7,4 +7,22 @@ class DrawingFormatConfiguration < ActiveRecord::Base
     attribute_names - ["id","user_id","show_grid"]
   end
   
+  def self.helptext(attribute_name)
+    case attribute_name
+      when "width" : "desired image width in pixel"
+      when "margins" : "space left and right of diagram, in pixels"
+      when "bar_height" : "height of a feature bar, in pixels"
+      when "bar_vspace" : "space between feature bars, in pixels"
+      when "track_vspace" : "space between tracks, in pixels"
+      when "scale_arrow_width" : "width of scale arrowheads, in pixels"
+      when "scale_arrow_height" : "height of scale arrowheads, in pixels"
+      when "arrow_width" : "width of feature arrowheads, in pixels"
+      when "stroke_width" : "width of outlines, in pixels"
+      when "stroke_marked_width" : "width of outlines for marked elements, in pixels"
+      when "show_grid" : "shows light vertical lines for orientation"
+      else
+	""
+    end
+  end
+  
 end
