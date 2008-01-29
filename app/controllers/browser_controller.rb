3b37db585bd62c39021a9e0280056a297b292b02
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-29 23:26:31 +0100

    fixed width related bug in public files
     - now if a session[:user] can't be found (read:
       you are not logged in), the width set by the
       owner of the file is used
     - another possibility would be to set it to 800
       in this case

--- app/controllers/browser_controller.rb
+++ app/controllers/browser_controller.rb
@@ -14,6 +14,7 @@ class BrowserController < ApplicationController
   partial = session[:user] ? "/in_session/navbar" : "/public/navbar"
   @navbar = (render_to_string :partial => partial)
   @width = session[:user] ? User.find(session[:user]).width : 800
+  @sequence_regions = Annotation.find(params[:annotation]).sequence_regions
  end
 
  def browser
@@ -69,13 +70,13 @@ class BrowserController < ApplicationController
   end
   #end checking params start_pos and end_pos
 
-  @width = User.find(session[:user]).width
+  @width = User.find(session[:user]).width rescue sequence_region.annotation.user.width
   render  :layout => false
  end
 
  def browser_image
-  user = User.find(session[:user])
   annotation=Annotation.find(params[:annotation])
+  user = User.find(session[:user]) rescue annotation.user
   sequence_region=SequenceRegion.find(params[:seq_id])
   png_data = sequence_region.to_png(params[:start_pos].to_i,
                                                         params[:end_pos].to_i, user.width)
