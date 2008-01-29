a109424e7299631f1a08ccc85a6fb8f32d53ffb4
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-30 00:22:04 +0100

    login: by failure stay on same page
     - on login failure a msg is displayed and
       the login page is reloaded
     - simplified controller code
     - deleted unreachable templates

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,30 +1,20 @@
 class LoginController < ApplicationController
-  
- def login
-   @page_title = "Genomeviewer - User Login"
- end
- def login_fail
-   @page_title = "Login Failed"
- end
- def login_ok
-   @page_title = "Login Successful"
+   
+  def login
+    @title="GenomeViewer - Login"
   end
- 
- 
- def do_login
- password_hash = Digest::SHA1.hexdigest(params[:password])
- user = User.find_by_email_and_password(params[:email], password_hash)
-  if user
-   session[:user]=user.id
-   redirect_to :controller => :in_session, :action => :index
-  else
-   redirect_to :controller => :login, :action => :login_fail
+   
+  def do_login
+    hashed_password = Digest::SHA1.hexdigest(params[:password])
+    user = User.find_by_email_and_password(params[:email], hashed_password)
+    if user
+      session[:user]=user.id
+      redirect_to :controller => :in_session, :action => :index
+    else
+      flash[:errors] = 
+        "Login failed!<br/>You have supplied invalid login information."
+      redirect_to :controller => :login, :action => :login
+    end
   end
- end
- 
- private
- def initialize
-  @title="GenomeViewer - Login"
-  super
- end
+   
 end
