84c2e56313a58d58eddc399e8dd27dad9243632c
Marvin Mundry <mmundry@oscar.mun>
2008-01-29 14:47:47 +0100

    Added public browsung to in_session

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,2 +1,17 @@
 class PublicController < ApplicationController
+ def showusers
+  partial = session[:user] ? "/in_session/navbar" : "/public/navbar"
+  @navbar = (render_to_string :partial => partial)
+ end
+
+ def showuserfiles
+  partial = session[:user] ? "/in_session/navbar" : "/public/navbar"
+  @navbar = (render_to_string :partial => partial)
+ end
+
+ private
+
+ def initialize
+  @stylesheets = "in_session"
+ end
 end
