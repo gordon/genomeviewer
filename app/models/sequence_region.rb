b18dd206d7ce56b856c969057d92df983741c853
Giorgio Gonnella <ggonnella@yahoo.it>
2008-01-30 02:52:31 +0100

    tried to add_introns
     - but lines 118 and 119 in gtserver are commented
       as the result is a seg fault and collapse of
       the drb server

--- app/models/sequence_region.rb
+++ app/models/sequence_region.rb
@@ -4,8 +4,8 @@ class SequenceRegion < ActiveRecord::Base
   # returns the png image representing the sequence region using the gt-ruby bindings
   def to_png(seq_begin = self.seq_begin, seq_end = self.seq_end, width = 800)
 
-    return GTSvr.getImageStream(File.expand_path(self.annotation.gff3_data_storage), seq_id, seq_begin, seq_end, annotation.user.config, width)
-    return render.to_png_stream(diagram, width)
+    return GTSvr.getImageStream(File.expand_path(self.annotation.gff3_data_storage), 
+               seq_id, seq_begin, seq_end, annotation.user.config, width, annotation.add_introns)
 
   end
 
