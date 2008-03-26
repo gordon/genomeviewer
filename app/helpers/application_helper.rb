1a8e985246b04c35ea02699bf2b0c80dc0eb7d9c
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-03-26 15:52:10 +0100

    cosmetics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,13 +13,13 @@ module ApplicationHelper
 
  def include_navbar
   # (1) control if the variable @navbar exists
-  return @navbar if @navbar 
+  return @navbar if @navbar
   # (2) control if the partial _navbar exists
   if File.exist?("app/views/#{controller.controller_name}/_navbar.html.erb")
-   return(render :partial => "navbar") 
+   return(render :partial => "navbar")
   end
   # (3) if both don't exist display nothing
-  return nil 
+  return nil
  end
 
 end
