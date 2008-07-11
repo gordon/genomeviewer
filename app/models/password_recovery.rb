0776f176829e4995059afc29e2aef4a04650e419
Giorgio Gonnella <ggonnella@yahoo.it>
2008-07-11 14:04:29 +0200

    added mailer method for password recovery
     example usage in controllers:
    
     PasswordRecovery.deliver_password_recovery_email_to(@user)

--- app/models/password_recovery.rb
+++ app/models/password_recovery.rb
@@ -1,4 +1,10 @@
 class PasswordRecovery < ActionMailer::Base
   
+  def password_recovery_email_to(user)
+    recipients  user.email
+    from        "password_recovery@genomeviewer.org"
+    subject     "Genomeviewer: Your Password"
+    body        :user => user
+  end
 
 end
