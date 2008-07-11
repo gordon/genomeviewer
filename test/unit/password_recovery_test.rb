0f987b94a2826979d8218997060a77a4c9a374f7
Giorgio Gonnella <ggonnella@yahoo.it>
2008-07-11 14:32:16 +0200

    added unit test for password recovery mailer
    to run the test
    
    (1) remember to create the database using rake db:test:prepare
    (2) ruby test/unit/password_recovery_test.rb

--- test/unit/password_recovery_test.rb
+++ test/unit/password_recovery_test.rb
@@ -1,9 +1,18 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class PasswordRecoveryTest < ActionMailer::TestCase
   tests PasswordRecovery
-  # replace this with your real tests
-  def test_truth
-    assert true
+  
+  def test_email_delivery
+    assert_no_emails
+    PasswordRecovery.deliver_password_recovery_email_to(user_for_testing)
+    assert_emails 1
   end
+  
+  def user_for_testing
+    @user ||= User.create(:name => "Test Person",
+                                     :email => "test@test.test",
+                                     :password => "test")
+  end
+  
 end
