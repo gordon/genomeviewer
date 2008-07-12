8198d3d2343fef005714a9773d5c5b718c1b4b7d
Giorgio Gonnella <ggonnella@yahoo.it>
2008-07-12 14:18:38 +0200

    changes in the layout
    - @title is displayed now both in the <title> tag of the page header
      and in a <h1> tag at the beginning of the page
    - this ensures that each page has a meaningful <title>
    - an helper method for the <title> tag was written,
      which adds "GenomeViewer" if the string is not already
      in @title, and in addition concatenates the content of @subtitle
      if this is present
    - a common place for messages (flash[:info], green and
      flash[:errors], red) was found, just after the initial h1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,20 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+  # a meaningful html <title> 
+  # it is used in application layout 
+  # in the header section
+  def title_tag
+    @subtitle = ": #@subtitle" if @subtitle
+    content_tag :title do 
+      if !@title
+        "GenomeViewer" 
+      elsif @title =~ /GenomeViewer/
+        "#@title#@subtitle"
+      elsif @title
+        "GenomeViewer - #@title#@subtitle"
+      end
+    end
+  end
+  
 end
