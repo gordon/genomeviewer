5aea73321a579c621be8affa8cba64773e8ff2cd
Giorgio Gonnella <ggonnella@yahoo.it>
2008-07-12 14:29:54 +0200

    register controller adapted to layout changes

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,11 +1,26 @@
 class RegisterController < ApplicationController
 
+  ### actions with a template ###
+
   def register
     # create a new user instance if here for the first time
     # or load it from the flash if coming back because of errors
+    @title = "Create a new account"
     @user = flash[:user]||User.new
   end
 
+  def recover_password
+    @title = "Recover your password"
+    @user = User.new(:email => flash[:email])
+  end
+  
+  def password_recovery_email_sent
+    @title = "Password recovered"
+    @user = User.find_by_email(flash[:email])
+  end
+  
+  ### actions redirecting to other actions ###
+  
   def do_register
     # create an user instance using the form parameters
     @user = User.new(params[:user])
@@ -23,17 +38,7 @@ class RegisterController < ApplicationController
       flash[:user] = @user
       redirect_to :action => :register
     end
-  end
-  
-  def recover_password
-    @title = "Genomeviewer - Password Recovery"
-    @user = User.new(:email => flash[:email])
-  end
-  
-  def password_recovery_email_sent
-    @title = "Genomeviewer - Password Recovery"
-    @user = User.find_by_email(flash[:email])
-  end
+  end  
   
   def send_password_recovery_email
     flash[:email]=params[:user][:email]
@@ -47,12 +52,4 @@ class RegisterController < ApplicationController
     end
   end
 
-  private
-
-  def initialize
-    @title = "Genomeviewer - User Registration"
-    super
-  end
-
 end
-
