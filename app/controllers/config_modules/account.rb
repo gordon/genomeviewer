7c0979c99005e52c9cee9175527985052b8a12cf
Giorgio Gonnella <ggonnella@yahoo.it>
2008-07-14 20:34:58 +0200

    configuration page to change password was added

--- app/controllers/config_modules/account.rb
+++ app/controllers/config_modules/account.rb
@@ -8,6 +8,11 @@ module ConfigModules::Account
     @user = User.find(session[:user])
   end
   
+  def config_password
+    @title = "Configuration"
+    @subtitle = "Password"
+  end
+
   def config_personal_info
     @title = "Configuration"
     @subtitle = "Personal Information"
@@ -31,13 +36,38 @@ module ConfigModules::Account
       else
         # error, go back to the form
         flash[:errors] = @user.errors[:email].kind_of?(String) ? 
-                         "#{@user.errors[:email]} (#{params[:user][:email]})" : 
-                            @user.errors[:email][0]
+                         "#{@user.errors[:email]} (#{params[:user][:email]})" :
+                         @user.errors[:email][0]
         redirect_to :action => :config_email
       end
     end
   end
-    
+  
+  def do_config_password
+    @user = User.find(session[:user])
+    if params[:old] == @user.password
+      # old password correct
+      if params[:new] == params[:confirm]
+        # confirmation correct => try to change password
+        if @user.update_attributes(:password => params[:new])
+          flash[:info] = "Your password was changed"
+          redirect_to configuration_url
+        else
+          flash[:errors] = @user.errors[:password]
+          redirect_to :action => :config_password
+        end
+      else
+        # confirmation failed => back
+        flash[:errors] = "You entered two different passwords"
+        redirect_to :action => :config_password
+      end
+    else
+      # old password was wrong => back
+      flash[:errors] = "Old password wrong"
+      redirect_to :action => :config_password
+    end
+  end
+  
   def do_config_personal_info
     @user = User.find(session[:user])
     # try to change parameters
