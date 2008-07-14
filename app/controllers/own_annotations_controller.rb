85d373b60ec5a6a0934b08b01ed7a4306729a68b
Giorgio Gonnella <ggonnella@yahoo.it>
2008-07-14 09:02:34 +0200

    own annotations: new file manager
    * active scaffold based
    * create action used for uploading
    * update action used for changing description
    * helper method for an ajax based file accessibility change
    * customized message if no gff3 files are present

--- app/controllers/own_annotations_controller.rb
+++ app/controllers/own_annotations_controller.rb
@@ -1,2 +1,74 @@
 class OwnAnnotationsController < ApplicationController
+  
+  prepend_before_filter :check_login
+  def check_login
+    redirect_to root_url unless session[:user]
+    return true
+  end
+  
+  active_scaffold :annotations do |config|
+  
+    config.actions.exclude :show
+    # create action will be used for uploads
+    config.create.link.label = "Upload GFF3"
+    # update action will be limited to file description
+    config.update.link.label = "Edit description"
+    config.create.multipart = true
+    config.list.columns = [:name, :description, :sequence_regions, :public]
+    config.columns[:public].label = "File&nbsp;Access"
+    config.action_links.add :open,
+                            :label => "Open",
+                            :type => :record, 
+                            :page => true
+                            
+  end
+  
+  def conditions_for_collection
+    ["user_id = ?", session[:user]]
+  end
+  
+  def open 
+    redirect_to :controller => :browser,
+                :action => :seq_id_select, 
+                :annotation => params["id"]
+  end
+
+  # upload callback
+  def before_create_save(record)
+    record.name = params[:record][:gff3_file].original_filename
+    record.user_id = session[:user]
+    record.gff3_data = params[:record][:gff3_file].read
+  end
+  
+  ### ajax actions ###
+  
+  def file_access_control
+    annotation = Annotation.find(params[:id])
+    user = User.find(session[:user])
+    # prevent others from modifying own data
+    if annotation.user == user
+      previously_public = annotation.public
+      annotation.public = (params[:checked]=="public")
+      if annotation.public
+        user.increment(:public_annotations_count) unless previously_public        
+      else # private
+        user.decrement(:public_annotations_count) if previously_public
+      end
+      ActiveRecord::Base.transaction do 
+        user.save
+        annotation.save
+      end
+      render :inline => '', :status => 200
+    else 
+      render :inline => "Authorization Error. Are you logged in?", :status => 500
+    end
+  end  
+
+  private
+  
+  def initialize
+    @title = "Own Annotations"
+    super
+  end
+    
 end
--- app/helpers/own_annotations_helper.rb
+++ app/helpers/own_annotations_helper.rb
@@ -1,2 +1,26 @@
 module OwnAnnotationsHelper
+  include PublicAnnotationsHelper
+  
+  def public_column(record)
+    make_it_private = remote_function :url => {:action => :file_access_control,
+                                               :id => record.id,
+                                               :checked => :private}
+    make_it_public  = remote_function :url => {:action => :file_access_control,
+                                               :id => record.id,
+                                               :checked => :public}
+    html = ""
+    html << (radio_button_tag "access_#{record.id}", 
+                              "private", !record.public,
+                              :id => "access_#{record.id}_private",
+                              :onclick => make_it_private)
+    html << "Private" 
+    html << tag(:br) 
+    html << (radio_button_tag "access_#{record.id}", 
+                              "public", record.public,
+                              :id => "access_#{record.id}_public",
+                              :onclick => make_it_public) 
+    html << "Public" 
+    return html
+  end
+  
 end
