41b7c1f4d716d303d82cc48ed09d250ec328c7cd
Giorgio Gonnella <ggonnella@yahoo.it>
2008-07-14 19:22:01 +0200

    added email to notify email change

--- test/unit/user_mailer_test.rb
+++ test/unit/user_mailer_test.rb
@@ -15,6 +15,12 @@ class UserMailerTest < ActionMailer::TestCase
     assert_emails 1
   end
   
+  def test_email_change
+    assert_no_emails
+    UserMailer.deliver_email_changed_message_to(user_for_testing)
+    assert_emails 1
+  end
+  
   def user_for_testing
     @user ||= User.create(:name => "Test Person",
                                      :email => "test@test.test",
