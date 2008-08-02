45883c1ab1312aceda238897f839259e5462af55
Giorgio Gonnella <ggonnella@yahoo.it>
2008-08-02 11:46:14 +0200

    added failsafe against gt server crash
    SequenceRegion#to_png will now continue forever to try to get an image from
    the gt server.
    
    If the gt server is called in an infinite loop, restarting itself after a
    failure, the whole thing will be almost transparent (apart from the delay)
    for the user.
    
    script/loop script/gt_server
    
    implements it for example with a minimal bash script.

--- app/models/sequence_region.rb
+++ app/models/sequence_region.rb
@@ -3,10 +3,12 @@ class SequenceRegion < ActiveRecord::Base
 
   # returns the png image representing the sequence region using the gt-ruby bindings
   def to_png(seq_begin = self.seq_begin, seq_end = self.seq_end, width = 800)
-
-    return GTServer.get_image_stream(File.expand_path(self.annotation.gff3_data_storage),
+    begin
+      return GTServer.get_image_stream(File.expand_path(self.annotation.gff3_data_storage),
                seq_id, seq_begin, seq_end, annotation.user.config, width, annotation.add_introns)
-
+    rescue
+      retry
+    end
   end
 
 end
