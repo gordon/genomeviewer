f6d5e9ae7952d4a365574a7c2f23e5aa6c0beb62
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-08-07 13:41:52 +0200

    src_clean

--- test/unit/gt_server_test.rb
+++ test/unit/gt_server_test.rb
@@ -8,32 +8,32 @@ class GtServerTest < ActiveSupport::TestCase
   end
 
   def test_get_sequence_regions
-    seq_ids = 
+    seq_ids =
      %w{X 1 2 5 6 7 8 9 10 11 12 13 14 15 16 18 19 20 21 22}.map{|n| "chr#{n}"}
     file = "test/gff3/encode_known_genes_Mar07.gff3"
     # don't test direct array equality as the order is not important:
     response = GTServer.get_sequence_regions(file)
     response.each_with_index{|e, i| response[i]=nil if seq_ids.delete(e)}
-    assert response.compact.empty? 
+    assert response.compact.empty?
     assert seq_ids.empty?
   end
-  
+
   def test_get_feature_classes
     f_classes = %w{mRNA gene exon intron TF_binding_site}
     file = "test/gff3/standard_gene_with_introns_as_tree.gff3"
     # don't test direct array equality as the order is not important:
     response = GTServer.get_feature_classes(file)
     response.each_with_index{|e, i| response[i]=nil if f_classes.delete(e)}
-    assert response.compact.empty? 
+    assert response.compact.empty?
     assert f_classes.empty?
   end
-  
+
   def test_get_range_for_sequence_region
     file = "test/gff3/little1.gff3"
-    assert_equal [1000, 9000], 
+    assert_equal [1000, 9000],
                  GTServer.get_range_for_sequence_region(file, "test1")
   end
-  
+
   def test_new_config_object
     assert_nothing_raised {GTServer.new_config_object}
   end
@@ -44,7 +44,7 @@ class GtServerTest < ActiveSupport::TestCase
     GTServer.config_object_for_user(1)
     assert GTServer.cached_config_for?(1)
   end
-  
+
   def test_independent_config_changes
     config1 = GTServer.config_object_for_user(1)
     config2 = GTServer.config_object_for_user(2)
@@ -53,14 +53,14 @@ class GtServerTest < ActiveSupport::TestCase
     config2.set_cstr("format", "margins", 40.to_s)
     assert GTServer.cached_config_for?(1)
     assert GTServer.cached_config_for?(2)
-    assert_not_equal config1.get_cstr("format", "margins"), 
+    assert_not_equal config1.get_cstr("format", "margins"),
                      config2.get_cstr("format", "margins")
   end
-  
+
   def test_new_color_object
     assert_nothing_raised {GTServer.new_color_object}
   end
-    
+
   def test_get_image_stream
     file = "test/gff3/encode_known_genes_Mar07.gff3"
     config = GTServer.default_config_object
@@ -68,7 +68,7 @@ class GtServerTest < ActiveSupport::TestCase
     assert png_data.size > 1000
     assert_equal "PNG", png_data[1..3]
   end
-  
+
   def test_get_image_map
     file = "test/gff3/little1.gff3"
     config = GTServer.default_config_object
@@ -78,7 +78,7 @@ class GtServerTest < ActiveSupport::TestCase
       assert_equal "gene1", feat.get_attribute("ID")
     end
   end
-  
+
   #
   # note: GTServer.validate_file() does not raise
   # exceptions, but rather returns nil if the file
@@ -88,7 +88,7 @@ class GtServerTest < ActiveSupport::TestCase
     valid_gff3 = "test/gff3/little2.gff3"
     assert_nil GTServer.validate_file(valid_gff3)
   end
-  
+
   def test_validate_invalid
     invalid_gff3 = "tmp/~deleteme"
     File.open(invalid_gff3,"w+") {|f| f.puts "nonsense"}
@@ -101,7 +101,7 @@ class GtServerTest < ActiveSupport::TestCase
     GTServer.get_feature_index_for_file(file, :delete_cache => true)
     assert !GTServer.cached_feature_index_for?(file)
     GTServer.get_feature_index_for_file(file)
-    assert GTServer.cached_feature_index_for?(file)    
+    assert GTServer.cached_feature_index_for?(file)
   end
 
 end
