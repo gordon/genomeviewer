a817b72e64bc9c29ea0c682486cb5849982ce1da
Giorgio Gonnella <ggonnella@yahoo.it>
2008-08-09 13:57:42 +0200

    applied GTRubyConfigurator to Format class

--- app/models/format.rb
+++ app/models/format.rb
@@ -1,28 +1,32 @@
 class Format < ActiveRecord::Base
-
+  
   belongs_to :configuration
-
-  # list of attribute names that have a value expressed in pixel
-  def pixel_attribute_names
-    attribute_names - ["id","user_id","show_grid"]
-  end
-
+  
+  include GTRubyConfigurator 
+  set_section "format"
+  set_colors :track_title_color, :default_stroke_color
+  set_bools :show_grid
+  set_floats :margins, :bar_height, :bar_vspace, :track_vspace, 
+         :scale_arrow_width, :scale_arrow_height, :arrow_width,
+         :stroke_width, :stroke_marked_width, :min_len_block
+  
   def self.helptext(attribute_name)
-    case attribute_name
-      when "width" : "(desired image width)"
-      when "margins" : "(space left and right of diagram)"
-      when "bar_height" : "(height of a feature bar)"
-      when "bar_vspace" : "(space between feature bars)"
-      when "track_vspace" : "(space between tracks)"
-      when "scale_arrow_width" : "(width of scale arrowheads)"
-      when "scale_arrow_height" : "(height of scale arrowheads)"
-      when "arrow_width" : "(width of feature arrowheads)"
-      when "stroke_width" : "(width of outlines)"
-      when "stroke_marked_width" : "(width of outlines for marked elements)"
-      when "show_grid" : "(shows light vertical lines for orientation)"
-      when "min_len_block" : "(minimum length of a block in which single elements are shown)"
+    case attribute_name.to_sym
+      when :margins : "space left and right of diagram"
+      when :bar_height : "height of a feature bar"
+      when :bar_vspace : "space between feature bars"
+      when :track_vspace : "space between tracks"
+      when :scale_arrow_width : "width of scale arrowheads"
+      when :scale_arrow_height : "height of scale arrowheads"
+      when :arrow_width : "width of feature arrowheads"
+      when :stroke_width : "width of outlines"
+      when :stroke_marked_width : "width of outlines for marked elements"
+      when :show_grid : "shows light vertical lines for orientation"
+      when :min_len_block : "minimum length of a block in which single elements are shown"
+      when :track_title_color : "color of the track title"
+      when :default_strock_color : "default stroke color"
       else
-	""
+	      ""
     end
   end
 
--- test/unit/format_test.rb
+++ test/unit/format_test.rb
@@ -5,65 +5,67 @@ class FormatTest < ActiveSupport::TestCase
   fixtures :users
   
   def setup
-    @user = users("a_test")
-    @format = FormatTest.new
-    @user.format_test = @format    
+    user = users("a_test")
+    user.reset_configuration
+    @conf = user.configuration
+    @format = Format.new
+    @conf.format = @format
+    @a_color = Color.new(0.1,0.2,0.3)
+    @a_float = 999.9
   end
   
-  def test_change_arrow_width
-    assert_not_equal 10.0, @user.config.get_num("format","arrow_width")
-    @format.arrow_width = 10
-    @user.flush_config_cache
-    assert_equal 10.0, @user.config.get_num("format","arrow_width")
+  def test_section
+    assert "format", @format.section
+  end
+  
+  def test_attribute_lists
+    # test one of the lists
+    colors = [:track_title_color, :default_stroke_color]
+    assert_equal colors, Format.list_colors 
+    # test global list
+    require "set" # use Set as the sorting order is not important
+    all = Format::ConfigTypes.map{|t| Format.send("list_#{t}")}.flatten
+    assert_equal all.to_set, Format.configuration_attributes.to_set
+  end
+  
+  def test_show_grid
+    args = ["format","show_grid"]
+    assert_equal @conf.gt.get_bool(*args), @format.default_show_grid
+    assert_not_equal false, @format.show_grid
+    assert_not_equal false, @conf.gt.get_bool(*args)
+    @format.show_grid = false
+    assert_equal false, @format.show_grid
+    assert_equal false, @conf.gt.get_bool(*args)
+  end
+
+  def test_tests_will_be_defined
+    assert Format.list_floats.size > 0
+    assert Format.list_colors.size > 0
+  end
+
+  Format.list_floats.each do |f|
+    define_method "test_#{f}" do
+    args = ["format",f.to_s]
+    assert_equal @conf.gt.get_num(*args), @format.send("default_#{f}")
+    assert_not_equal @a_float, @format.send(f)
+    assert_not_equal @a_float, @conf.gt.get_num(*args)
+    @format.send("#{f}=", @a_float)
+    assert_equal @a_float, @format.send(f)
+    assert_equal @a_float, @conf.gt.get_num(*args)     
+    end
+  end
+  
+  Format.list_colors.each do |col|
+    define_method "test_#{col}" do
+    args = ["format",col.to_s]
+    assert_equal Color(@conf.gt.get_color(*args)), 
+                 @format.send("default_#{col}")
+    assert_not_equal @a_color, @format.send(col)
+    assert_not_equal @a_color, Color(@conf.gt.get_color(*args))
+    @format.send("#{col}=", @a_color)
+    assert_equal @a_color, @format.send(col)
+    assert_equal @a_color, Color(@conf.gt.get_color(*args))   
+    end
   end
     
-  def test_change_bar_height
-    assert_not_equal 11.0, @user.config.get_num("format","bar_height")
-    @format.bar_height = 11
-    @user.flush_config_cache
-    assert_equal 11.0, @user.config.get_num("format","bar_height")
-  end
-
-  def test_change_margins
-    assert_not_equal 13.0, @user.config.get_num("format","margins")
-    @format.margins = 13
-    @user.flush_config_cache
-    assert_equal 13.0, @user.config.get_num("format","margins")
-  end
-  
-  def test_change_scale_arrow_height
-    assert_not_equal 1.1, @user.config.get_num("format","scale_arrow_height")
-    @format.scale_arrow_height = 1.1
-    @user.flush_config_cache
-    assert_equal 1.1, @user.config.get_num("format","scale_arrow_height")
-  end
-
-  def test_change_scale_arrow_width
-    assert_not_equal 1.2, @user.config.get_num("format","scale_arrow_width")
-    @format.scale_arrow_width = 1.2
-    @user.flush_config_cache
-    assert_equal 1.2, @user.config.get_num("format","scale_arrow_width")
-  end
-  
-  def test_change_show_grid
-    assert_not_equal "no", @user.config.get_cstr("format","show_grid")
-    @format.show_grid = false
-    @user.flush_config_cache
-    assert_equal "no", @user.config.get_cstr("format","show_grid")
-  end
-  
-  def test_change_stroke_width
-    assert_not_equal 1.4, @user.config.get_num("format","stroke_width")
-    @format.stroke_width = 1.4
-    @user.flush_config_cache
-    assert_equal 1.4, @user.config.get_num("format","stroke_width")
-  end
-  
-  def test_change_min_len_block
-    assert_not_equal 0.0, @user.config.get_num("format", "min_len_block")
-    @format.min_len_block = 0.0
-    @user.flush_config_cache
-    assert_equal 0.0, @user.config.get_num("format", "min_len_block")
-  end
-
 end
