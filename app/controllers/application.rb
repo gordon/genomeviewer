dcc12526fa9b9d26d702c7aaabd86756739f3350
Giorgio Gonnella <ggonnella@yahoo.it>
2008-08-11 20:31:34 +0200

    application controller: added get_current_user and enforce_login
    @@current_user: will contain always the current User
                    or nil if not logged in
    
    enforce_login: centralized login checker filter, use it as a before_filter
                   (so that it comes after get_current_user)

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,4 +4,17 @@
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_genomeviewer_session_id'
+  
+  prepend_before_filter :get_current_user
+  
+  private
+  
+  def get_current_user
+    @current_user = session[:user] ? User.find(session[:user]) : nil
+  end
+  
+  def enforce_login
+    redirect_to root_url unless @current_user
+  end
+
 end
