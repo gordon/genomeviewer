508047b35c5a5facaaf719a3a9cc274b03ddbf08
Giorgio Gonnella <ggonnella@yahoo.it>
2008-08-11 13:42:29 +0200

    initializer for value objects classes (Color, Style)

---
+++ config/initializers/value_objects.rb
@@ -0,0 +1,8 @@
+# 
+# as the value object classes used for aggregation mapping
+# define some class/module methods outside themselves
+# (i.e. in String and Kernel), assure their code is loaded
+# by listing them here
+#
+require "app/models/color.rb"
+require "app/models/style.rb"
