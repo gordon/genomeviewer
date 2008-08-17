ef51db0646911d40a49df9993d4e939cc0560281
Giorgio Gonnella <ggonnella@yahoo.it>
2008-08-17 13:37:31 +0200

    Configuration: added correct support for GT::Config caching
    when the object does not come from the cache, settings of all
    sections are uploaded
    
    gt method has now two facultative parameters, a section name and
    attribute name: the corresponding section does not upload the
    corresponding attribute; this is used by GTRubyConfiguration to
    avoid circular method calls

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -3,9 +3,17 @@ class Configuration < ActiveRecord::Base
   belongs_to :user  
   has_many :feature_types, :dependent => :destroy 
   has_one  :format, :dependent => :destroy
+  
+  def section_objects
+    [format]+feature_types
+  end  
+  
+  def sections
+    section_objects.map(&:section)
+  end
 
-  after_save :default_format
-  after_create :uncache
+  after_save       :default_format
+  after_create     :uncache
   
   def default_format
     unless Format.find_by_configuration_id(self[:id])
@@ -15,22 +23,34 @@ class Configuration < ActiveRecord::Base
     end
   end
 
-  # pointer to the gt_ruby GT::Config object in the DRb server
-  def gt
-    GTServer.config(config_key)
+  # reference to the GT::Config object 
+  # corresponding to this object
+  #
+  # if there is no cache, upload all setting
+  # from the DB into the gt config object
+  # but allow an exception to avoid circular 
+  # references
+  #
+  def gt(upload_exception_section = nil, upload_exception_attr = nil)
+    if !GTServer.config_cached?(self[:id])
+      GTServer.config(self[:id])
+      section_objects.each do |x|
+        if x.section == upload_exception_section
+          x.upload_except(upload_exception_attr)
+        else
+          x.upload
+        end
+      end
+    end
+    GTServer.config(self[:id])
   end
-
+  
   def uncache
-    GTServer.config_uncache(@key)
-    @key = nil
+    GTServer.config_uncache(self[:id])
   end
 
   def self.default
     GTServer.config_default
-  end
-
-  def config_key
-    @key ||= (self[:id] || self.object_id)
   end
 
 end
