e2541fd009be8e5580470d7a56fc759f93a4a89d
Giorgio Gonnella <ggonnella@yahoo.it>
2008-08-19 13:14:04 +0200

    FType Controller: color squares in list view

--- app/helpers/feature_types_helper.rb
+++ app/helpers/feature_types_helper.rb
@@ -1,8 +1,16 @@
 module FeatureTypesHelper
-  
+    
   def style_form_column(record, input_name)
     opts = options_for_select(["default"]+Style::DefinedStyles.values, record.style.to_s)
     select_tag input_name, opts
+  end
+  
+  FeatureType.list_colors.each do |c|
+    define_method "#{c}_column" do |record|
+      content_tag :div, '&nbsp;',
+        :class => 'square',
+        :style => "background-color: #{record.send(c)};"
+    end
   end
   
 end
