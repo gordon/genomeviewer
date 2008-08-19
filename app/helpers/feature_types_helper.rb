8e5f0186bfef7689275b3ee495d5f4d72d6a8c02
Giorgio Gonnella <ggonnella@yahoo.it>
2008-08-19 12:51:26 +0200

    Ftypes controller: styles chosen from select box

--- app/helpers/feature_types_helper.rb
+++ app/helpers/feature_types_helper.rb
@@ -1,2 +1,8 @@
 module FeatureTypesHelper
+  
+  def style_form_column(record, input_name)
+    opts = options_for_select(["default"]+Style::DefinedStyles.values, record.style.to_s)
+    select_tag input_name, opts
+  end
+  
 end
