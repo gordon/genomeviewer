a00c5d465dfe22cc7bb10fc5deb981e9da482a6b
Giorgio Gonnella <ggonnella@yahoo.it>
2008-10-10 17:34:25 +0200

    Functional tests for ConfigurationController

--- test/functional/configuration_controller_test.rb
+++ test/functional/configuration_controller_test.rb
@@ -1,5 +1,24 @@
 require 'test_helper'
 
 class ConfigurationControllerTest < ActionController::TestCase
+  
+  fixtures 'users'
+    
+  def setup
+    @u = users('a_test')
+    # session:
+    @in = {:user => @u.id}
+    @out = {}
+  end
+  
+  def test_unlogged_access_refused
+    get :index, {}, @out
+    assert_redirected_to root_url
+  end
+  
+  def test_logged_in_access
+    get :index, {}, @in
+    assert_template "configuration/index"
+  end
     
 end
