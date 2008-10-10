447f2fd203a314a718d23689a58017bd3eb11f8a
Giorgio Gonnella <ggonnella@yahoo.it>
2008-10-10 16:29:18 +0200

    Functional tests for DefaultController

--- test/functional/default_controller_test.rb
+++ test/functional/default_controller_test.rb
@@ -1,18 +1,39 @@
-require File.dirname(__FILE__) + '/../test_helper'
-require 'default_controller'
+require 'test_helper'
 
-# Re-raise errors caught by the controller.
-class DefaultController; def rescue_action(e) raise e end; end
+class DefaultControllerTest < ActionController::TestCase
+  
+  fixtures 'users'
 
-class DefaultControllerTest < Test::Unit::TestCase
   def setup
-    @controller = DefaultController.new
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new
+    @u = users('a_test')
+    @login_params = {:user => {:username => @u.username, 
+                               :password => @u.password}}
+    # session hash:
+    @in = {:user => @u.id}
+    @out = {}
   end
-
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  
+  def test_homepage
+    get :index
+    assert_response :success
+    assert_template "default/index"
   end
+  
+  def test_do_login
+    get :index
+    assert_nil assigns(:current_user)
+    post :do_login, @login_params
+    get :index
+    assert_equal @u, assigns(:current_user)
+  end
+  
+  def test_do_logout
+    post :do_login, @login_params
+    get :index
+    assert_equal @u, assigns(:current_user)
+    get :do_logout
+    get :index
+    assert_nil assigns(:current_user)
+  end
+  
 end
