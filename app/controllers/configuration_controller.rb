57cdd7d3dcbc9763b11c6a66ac8c000e15cc1552
Giorgio Gonnella <ggonnella@yahoo.it>
2008-10-10 17:32:04 +0200

    ConfigurationController documentation added

--- app/controllers/configuration_controller.rb
+++ app/controllers/configuration_controller.rb
@@ -1,8 +1,31 @@
+#
+# This controller is responsible for the user-specific 
+# settings page. 
+# 
+# The login is enforced, as these settings have a 
+# meaning only if an user is logged in. 
+#
+# It is actually only a "thin" controller with only 1 action (index)
+# embedding two active scaffolds (feature_types and format) that 
+# have each its own controller
+#
 class ConfigurationController < ApplicationController
   
   before_filter :enforce_login
   append_before_filter :title
   
+  ### actions with a template ###
+  
+  #
+  # configuration page, embedding feature_types and format active scaffolds
+  #
+  # see +FeatureTypesController+ and +FormatController+
+  #
+  def index
+  end
+
+private
+
   def title
     @title = "Configuration"
   end
