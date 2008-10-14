97ebdb87ed3ded48d08a463a1b7b58b4df9bb4c6
Giorgio Gonnella <ggonnell@yahoo.it>
2008-10-14 14:53:14 +0200

    FormatController: secured, documented, tested

--- app/controllers/format_controller.rb
+++ app/controllers/format_controller.rb
@@ -1,29 +1,63 @@
+#
+# This controller is responsible for an active_scaffold
+# (see vendor/plugins/active_scaffold) that is embedded 
+# in the configuration page.
+#
 class FormatController < ApplicationController
   
   before_filter :enforce_login
 
+  #
+  # declaration and configuration of the scaffold
+  #
   active_scaffold :format do |config|
     config.label = "General settings"
+    
+    # the columns array is built dinamically, using the lists
+    # provided by the FeatureType model
     columns = [:width] + Format.configuration_attributes
+    
     config.columns = columns
     config.columns[:width].label = "Default width"
+    
     columns.each do |c|
+      # disable sorting, as there is only 1 record
       config.columns[c].sort = false
+      # show an help text for each column
       config.columns[c].description = Format.helptext(c)
     end
+    # exclude unnecessary actions (increasing security)
     [:create, :search, :new, :delete, :show].each do |act|
       config.actions.exclude act
     end
 
   end
-  
+
+private
+
+  #
+  # show only current_user's record in list
+  #
   def conditions_for_collection
     ["configuration_id = ?", @current_user.configuration.id]
   end
   
+  #
+  # upload configuration changes in the gtserver 
+  # and save width
+  #
   def before_update_save(record)
     record.upload
     record.configuration.save # to save the width
   end
+  
+  #
+  # records should be edited only by their owner
+  #
+  def own_record?
+    record_owner = Format.find(params["id"]).configuration.user
+    redirect_to logout_url unless record_owner == current_user
+  end
+  alias_method :update_authorized?, :own_record?
   
 end
--- test/functional/format_controller_test.rb
+++ test/functional/format_controller_test.rb
@@ -1,5 +1,69 @@
 require 'test_helper'
 
 class FormatControllerTest < ActionController::TestCase
+  
+  fixtures 'users'
+  
+  def setup
+    assert user_setup # see test_helper
+    @_another_u = users('a_test')
+    @_another_u.configuration = Configuration.new
+  end
+  
+  def test_unlogged_access_refused
+    %w[index list edit destroy show new show_search].each do |x|
+      get x
+      assert_redirected_to root_url
+    end
+  end
+  
+  def test_list_logged_in
+    get :index, {}, {:user => @_u.id}
+    assert_template "format/list"
+  end
+  
+  def test_edit_own_format
+    get :edit, {:id => @_f.id}, {:user => @_u.id}
+    assert_template "format/update_form"
+    put :update, {:id => @_f.id,
+                  "record"=>{"margins" => 10}}, 
+                  {:user => @_u.id}
+    # test if the record was updated:
+    assert_equal 10, Format.find(@_f.id).margins
+  end
+  
+  def test_edit_without_being_logged_in
+    get :edit, :id => 1
+    assert_redirected_to root_url
+    put :update, {:id => @_f.id,
+                  "record"=>{"margins" => 10}}
+    assert_redirected_to root_url
+    assert_not_equal 10, Format.find(@_f.id).margins
+  end
+  
+  def test_edit_not_own_format
+    get :edit, {:id => @_f.id}, {:user => @_another_u.id}
+    assert_redirected_to logout_url
+    post :update, {:id => @_f.id,
+                  "record"=>{"margins" => 10}}, 
+                 {:user => @_another_u.id}
+    assert_redirected_to logout_url
+    assert_not_equal 10, Format.find(@_f.id).margins
+  end
+  
+  def test_delete_disabled
+    assert_raises(ActionController::UnknownAction) do 
+      get :destroy, {:id => @_f.id}, {:user => @_u.id}
+    end
+    assert_raises(ActionController::UnknownAction) do 
+      get :delete, {:id => @_f.id}, {:user => @_u.id}
+    end
+  end
+    
+  def test_show_disabled
+    assert_raises(ActionController::UnknownAction) do 
+      get :show, {:id => @_f.id}, {:user => @_u.id}
+    end
+  end
     
 end
