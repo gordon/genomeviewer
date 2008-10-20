df947c985ce31720adbb97ec1b96a55f41f81514
Giorgio Gonnella <ggonnell@yahoo.it>
2008-10-20 18:11:52 +0200

    RegisterController functional test [#10]
    [#10 state:resolved]

--- test/functional/register_controller_test.rb
+++ test/functional/register_controller_test.rb
@@ -1,18 +1,52 @@
-require File.dirname(__FILE__) + '/../test_helper'
-require 'register_controller'
+require 'test_helper'
 
-# Re-raise errors caught by the controller.
-class RegisterController; def rescue_action(e) raise e end; end
+class RegisterControllerTest < ActionController::TestCase
 
-class RegisterControllerTest < Test::Unit::TestCase
-  def setup
-    @controller = RegisterController.new
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new
+  def test_register
+    get :register
+    assert_template "register/register"
+  end
+  
+  def test_do_register
+    # be sure the user wasn't already there:
+    assert !User.find_by_username("test_username")
+    post :do_register, 
+         :user => {"name"                  => "Test Name", 
+                   "institution"           => "Test Institute", 
+                   "password"              => "test_pass", 
+                   "password_confirmation" => "test_pass", 
+                   "username"              => "test_username", 
+                   "url"                   => "www.test-institute.edu", 
+                   "email"                 => "test_email@mytest.tst"}
+    # test the user was really created:
+    assert User.find_by_username("test_username")
+  ensure
+    User.find_by_username("test_username").destroy
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_recover_password
+    get :recover_password
+    assert_template "register/recover_password"
   end
+  
+  def test_send_password_recovery_email_negative
+    post :send_password_recovery_email, :user => {:email => "aha"}
+    assert_redirected_to "register/recover_password"
+    assert_not_nil flash[:errors]
+  end
+  
+  def test_send_password_recovery_email_positive
+    user_setup
+    post :send_password_recovery_email, :user => {:email => @_u.email}
+    assert_redirected_to "register/password_recovery_email_sent"
+    assert_nil flash[:errors]
+  end
+  
+  def test_password_recovery_email_sent
+    user_setup # see test_helper
+    flash = {:email => @_u.email}
+    get :password_recovery_email_sent, {}, {}, flash
+    assert_template "register/password_recovery_email_sent"
+  end
+  
 end
