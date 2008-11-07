fe7e052a3708710a1e4b4f94a0fe8b14f225e600
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-11-07 18:08:09 +0100

    some email wording changes
    support@genomeviewer is now a valid email address

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -2,21 +2,21 @@ class UserMailer < ActionMailer::Base
 
   def password_recovery_email_to(user)
     recipients  user.email
-    from        "password_recovery@genomeviewer.org"
-    subject     "GenomeViewer: Your Password"
+    from        "support@genomeviewer.org"
+    subject     "GenomeViewer: Your reminder"
     body        :user => user
   end
 
   def signup_notification_to(user)
     recipients  user.email
-    from        "signup_notification@genomeviewer.org"
+    from        "support@genomeviewer.org"
     subject     "GenomeViewer: Thank you for registering"
     body        :user => user
   end
 
   def email_changed_message_to(user)
     recipients  user.email
-    from        "accounts@genomeviewer.org"
+    from        "support@genomeviewer.org"
     subject     "GenomeViewer: Email address changed"
     body        :user => user
   end
